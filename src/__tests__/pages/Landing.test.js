3afa5fa888e95b1f3c0ec012690ddc2909e3af03
Ian Roinson <ian@englishteeth.com>
2020-06-20 13:52:37 +0300

    Adding Shape Hero component

--- src/__tests__/pages/Landing.test.js
+++ src/__tests__/pages/Landing.test.js
@@ -3,6 +3,9 @@ import { shallow } from "enzyme";
 import Landing from "../../pages/Landing";
 
 import NavigationBar from "../../components/nav/NavigationBar"
+import ShapeHero from "../../components/sections/ShapeHero"
+import ProductSection from "../../components/sections/ProductSection"
+
 
 describe("Landing Page Testing", () => {
   let wrapper;
@@ -11,11 +14,19 @@ describe("Landing Page Testing", () => {
   });
 
   test("render the page", () => {
-    expect(wrapper.find("main").text()).toContain("I am Landing!");
+    expect(wrapper).not.toBeEmptyRender();
   });
 
   test("Navigation Bar should be present", () => {
     expect(wrapper.find(NavigationBar)).toExist();
+  });
+
+  test("Hero Section should be present", () => {
+    expect(wrapper.find(ShapeHero)).toExist();
+  });
+
+  test("Product Section should be present", () => {
+    expect(wrapper.find(ProductSection)).toExist();
   });
 
 });
---
+++ src/components/sections/ProductSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {
+} from "reactstrap";
+
+const ProductSection = (props) => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default ProductSection;
