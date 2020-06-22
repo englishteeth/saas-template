350cf5270a5c0b666fccaff9007b8bb743563023
Ian Roinson <ian@englishteeth.com>
2020-06-22 16:22:15 +0300

    Just enough footer to render something on the page

--- src/__tests__/components/nav/Footer.test.js
+++ src/__tests__/components/nav/Footer.test.js
@@ -10,6 +10,19 @@ describe("Footer Component Testing", () => {
 
   test("Footer should render", () => {
     expect(wrapper).not.toBeEmptyRender();
+    const footer = wrapper.find('footer');
+    expect(footer).toExist();
+  });
+
+  test("Include copyright in footer", () => {
+    const company={name: 'test corp', link: 'https://www.test-corp.com?ref=saas-template'}
+    wrapper = shallow(<Footer company={company} />);
+    const copyright = wrapper.find('.copyright');
+    expect(copyright).toExist();
+    expect(copyright).toIncludeText("©");
+    expect(copyright).toIncludeText(new Date().getFullYear());
+    expect(copyright.find('a')).toHaveProp("href", company.link);
+    expect(copyright.find('a')).toHaveText(company.name);
   });
 
 });
--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -1,13 +1,36 @@
 import React from "react";
 
 import {
+  Container, Row, Col
 } from "reactstrap";
 
 const Footer = (props) => {
   return (
     <>
+    <footer className="footer">
+    <Container>
+      <Row className="row-grid align-items-center mb-5"></Row>
+      <hr />
+      <Row className="align-items-center justify-content-md-between">
+        <Col md="6">
+          <div className="copyright">
+            © {new Date().getFullYear()}{" "}
+            <a href={props.company.link} target="_blank" rel="noopener noreferrer" >{props.company.name}</a>
+            .
+          </div>
+        </Col>
+      </Row>
+    </Container>
+    </footer>
     </>
   );
+}
+
+Footer.defaultProps = {
+  company: {
+    name: "No Company",
+    link: "#no-link"
+  }
 }
 
 export default Footer;
