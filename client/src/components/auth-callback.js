d3a82ca8f1e72ef2846e185f2961a1f6f2d2f307
Ian Roinson <ian@englishteeth.com>
2020-07-03 12:04:36 +0300

    Working on UI/API auth flow

--- client/src/components/auth-callback.js
+++ client/src/components/auth-callback.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 import { useAuthentication } from "../providers/authentication-context";
 
 export const AuthCallback = () => {
-  const {signinRedirectCallback} = useAuthentication();
+  const { signinRedirectCallback } = useAuthentication();
   const history = useHistory();
   console.log("callback route ");
   signinRedirectCallback((route) => history.push(route));
